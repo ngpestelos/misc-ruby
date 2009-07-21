de0d58093240d8540866628a3481b0ff8cdf2118
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-07-21 12:17:12 +0800

    for loop

---
+++ basic/for.rb
@@ -0,0 +1,13 @@
+# See Programming Ruby, p. 97
+
+for i in ['fee', 'fi', 'fo', 'fum']
+  print i, " "
+end
+
+for i in 1..3
+  print i, " "
+end
+
+for i in File.open("ordinal").find_all { |line| line =~ /d$/ }
+  print i.chomp, " "
+end 
