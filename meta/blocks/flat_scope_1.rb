9b0dfdb47cb971eec01e9bccf39f54e54fd6e7af
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-11-25 16:25:50 +0800

    flat scope

---
+++ meta/blocks/flat_scope_1.rb
@@ -0,0 +1,9 @@
+my_var = "Success"
+
+class MyClass
+  # We want to print my_var here...
+
+  def my_method
+    # ...and here
+  end
+end
---
+++ meta/blocks/flat_scope_2.rb
@@ -0,0 +1,9 @@
+my_var = "Success"
+
+MyClass = Class.new do
+  puts "#{my_var} in the class definition!"
+
+  def my_method
+    # how to print here? 
+  end
+end
