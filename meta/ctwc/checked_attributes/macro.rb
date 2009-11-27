ec5148fc7a13d7882f64cf5c32a9be7954e6e89e
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-11-27 10:52:34 +0800

    step four

---
+++ meta/ctwc/checked_attributes/macro.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+
+class Class
+  def attr_checked(attribute, &validation)
+    define_method "#{attribute}=" do |val|
+      raise 'Invalid attribute' unless validation.call(val)
+      instance_variable_set("@#{attribute}", val)
+    end
+    
+    define_method attribute do
+      instance_variable_get "@#{attribute}"
+    end
+  end
+end
+
+class Person
+  attr_checked :age do |val|
+    val >= 18
+  end
+end
+
+class TestCheckedAttribute < Test::Unit::TestCase
+  def setup
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_invalid_values
+    assert_raises RuntimeError, "Invalid attribute" do
+      @bob.age = 17
+    end
+  end
+
+end
