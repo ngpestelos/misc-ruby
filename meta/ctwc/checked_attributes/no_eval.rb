bef1ee75733c5b1af94c056aae901ef162977749
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-11-27 10:38:52 +0800

    no eval (step two)

---
+++ meta/ctwc/checked_attributes/no_eval.rb
@@ -0,0 +1,40 @@
+require 'test/unit'
+
+class Person; end
+
+class TestCheckedAttribute < Test::Unit::TestCase
+  def setup
+    add_checked_attribute(Person, :age)
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_nil_values
+    assert_raises RuntimeError, "Invalid attribute" do
+      @bob.age = nil
+    end
+  end
+
+  def test_refuses_false_values
+    assert_raises RuntimeError, "Invalid attribute" do
+      @bob.age = false
+    end
+  end
+end
+
+def add_checked_attribute(clazz, attribute)
+  clazz.class_eval do
+    define_method "#{attribute}=" do |val|
+      raise 'Invalid attribute' unless val
+      instance_variable_set("@#{attribute}", val)
+    end
+
+    define_method attribute do
+      instance_variable_get "@#{attribute}"
+    end
+  end
+end
