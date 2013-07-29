060f7360a7f3aa858e870b5e2a476dd592b7c2f2
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-07-30 05:20:57 +0800

    define a to_s method

--- flanagan/7-modules/point.rb
+++ flanagan/7-modules/point.rb
@@ -2,10 +2,17 @@ class Point
   def initialize(x=0, y=0)
     @x, @y = x, y
   end
+
+  def to_s
+    "{#@x, #@y}"
+  end
 end
 
 p = Point.new
+puts p
 puts p.class
 puts p.is_a? Point
 
-puts Point.new(1, 1).inspect
+p = Point.new(1, 1)
+puts p
+puts p.inspect
