7659904c94ec93511e762ae8d1804b88f6f60943
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-08-04 11:41:21 +0800

    contrast class variables with class instance variables

--- flanagan/7-modules/lib/class_variables.rb
+++ flanagan/7-modules/lib/class_variables.rb
@@ -1,14 +1,22 @@
 class A
   @@value = 1
+  @instance_value = 'a'
   def A.value; @@value; end
+  def A.instance_value; @instance_value; end
 end
 
-puts "A (initial state) #{A.value}" # 1
+puts "A initial state: #{A.value} #{A.instance_value}" # 1 a
 
-class B < A; @@value = 2; end
+# Subclass mutates its superclass' class variable,
+# but not the class instance variable
 
-puts "A (after B assignment) #{A.value}" # 2
+class B < A; @@value = 2; @instance_value = 'b'; end
+puts "A after B: #{A.value} #{A.instance_value}" # 2 a
+puts "B: #{B.value} #{B.instance_value}" # 2 b
 
-class C < B; @@value = 3; end
+# Subclass yet again mutates its ancestor's class variable,
+# but not the class instance variable
 
-puts "A (after C assignment) #{A.value}" # 3
+class C < B; @@value = 3; @instance_value = 'c'; end
+puts "A after C: #{A.value} #{A.instance_value}" # 3 a
+puts "C: #{C.value} #{C.instance_value}" # 3 c
